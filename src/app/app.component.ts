42bffe3daab5ab54956ba3aab1d561b37c006743
ccgg1290local <ccgg1290@gmail.com>
2022-04-25 19:01:25 -0500

    Practica video 17Terminamos la pequeña práctica recopilatorio: agregamos el botón al formulario y construimos el método que responde al evento del botón.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'practica_app_empleados';
+  titulo = 'Listado de empleados';
+  
+  empleados:Empleado[]=[
+ 
+     new Empleado("claudi","rincon","presidente",7500),
+     new Empleado("ginna","meneses","presidente",8000),
+     new Empleado("sara","urrego","presidente",9000),
+     new Empleado("sofia","martinez","presidente",7700),
+    
+  ];
+
+  agregarEmpleado(){
+
+    let miEmpleado= new Empleado(this. cuadroNombre,this.cuadroApellido,this.cuadroCargo,this.cuadroSalario);
+    this.empleados.push(miEmpleado)
+
+  }
+
+
+  cuadroNombre:string="";
+  cuadroApellido:string="";
+  cuadroCargo:string="";
+  cuadroSalario:number=0;
+
+
 }
